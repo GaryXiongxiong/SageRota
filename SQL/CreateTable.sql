7b84af58b7d819c1e9ac8670070d35a050a55af6
Xinchi Yu (PGT) <b9042547@newcastle.ac.uk>
2020-02-07 23:00:10 +0000

    Update SQL/CreateTable.sql

--- SQL/CreateTable.sql
+++ SQL/CreateTable.sql
@@ -1,23 +1,23 @@
 CREATE TABLE IF NOT EXISTS `timetable`.`shift` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `staff_sid` int(11) NOT NULL,
-  `start_time` date NOT NULL,
+  `start_time` DATE NOT NULL,
   `end_time` DATE NOT NULL,
   `location` varchar(150) DEFAULT NULL COMMENT ' the place a staff working now',
   `remark` varchar(300) DEFAULT NULL COMMENT 'description of this work.(for example: night-shift,day-shift )',
   PRIMARY KEY (`id`),
   UNIQUE KEY `start_time_UNIQUE` (`start_time`),
   UNIQUE KEY `end_time_UNIQUE` (`end_time`),
-  INDEX `fk_timetable_Staff_idx` (`Staff_sid` ASC) VISIBLE,
+  INDEX `fk_timetable_Staff_idx` (`staff_sid` ASC) VISIBLE,
   CONSTRAINT `fk_timetable_Staff`
-    FOREIGN KEY (`Staff_sid`)
+    FOREIGN KEY (`staff_sid`)
     REFERENCES `timetable`.`staff` (`sid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 
 CREATE TABLE IF NOT EXISTS `timetable`.`staff` (
-  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `sid` int NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `phone_number` VARCHAR(12) NOT NULL,
