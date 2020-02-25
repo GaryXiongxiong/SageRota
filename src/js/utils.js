4f6f7e71bda590d8e29e59a8402f60c4d146478e
Yixiong Jiang <i@jiangyixiong.top>
2020-02-25 11:31:25 +0000

    Modification: Optimize utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,8 @@
 function getUrlParam(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-    var r = window.location.search.substr(1).match(reg);
-    if (r != null) return unescape(r[2]); return null;
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
 }
 
 function showLoading() {
